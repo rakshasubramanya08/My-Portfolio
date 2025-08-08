1ced0ebcc33982c6f5cb6436e4699626e6521f29
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-08-08 16:28:12 +0200

    Added information

---
+++ src/Info/Info.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Info.css';
+
+const Info = () => {
+  return (
+    <>
+    <div className="containor">
+    <div className="info1">
+      <img src='src/assets/images.jpeg' alt='img' width="220px" height="200px" />
+    </div>
+    <div className="info2">
+        <p>🏠16 Florianstr.</p>
+        <p>Metzingen, 72555</p>
+        <p>📱 +49-15563386243</p>
+        <p>✉️
+       rakshasubramanya08@gmail.com</p>     
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Info
