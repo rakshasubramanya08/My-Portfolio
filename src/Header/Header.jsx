a1ffee9dfa46ef472c76f1cc6349e76d298f7167
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-08-08 15:20:50 +0200

    Initially added file and base code

---
+++ src/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <>
+    <div className="containor1">
+    <div className='head1'>
+        <div className='name1'>
+            <h1>Raksha Bidadi Ravishankar</h1>
+            <h3>Frontend Developer</h3>
+        </div>
+        <div className="icon">
+            <img src='src/assets/girl icon.gif' alt='icon' width="180px" height="160px"/>
+        </div>
+     
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
