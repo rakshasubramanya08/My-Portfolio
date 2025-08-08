e57dc3856dbbb58e6928b75edef8f489f8345097
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-08-08 16:27:44 +0200

    Modified.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Header/Header'
+import Info from './Info/Info'
 
 function App()  {
   return (
     <div>
       <Header />
+      <br />
+      <Info />
       
     </div>
   )
