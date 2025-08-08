4c3694a080c7f8014d71de0794fc4705a30ebb58
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-08-08 17:43:55 +0200

    Modified and added image

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,14 +5,27 @@ const Header = () => {
   return (
     <>
     <div className='head1'>
+        <div className="leftInfo">
+            <ul className='leftlist'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
         <div className='name1'>
             <h1>Raksha Bidadi Ravishankar</h1>
             <h3>Frontend Developer</h3>
         </div>
         <div className="icon">
-            <img src='src/assets/girl icon.gif' alt='icon' width="180px" height="170px"/>
+            <img src='src/assets/girl icon.gif' alt='icon' width="190px" height="150px"/>
         </div>   
     </div> 
+    <div className='body1'>
+        <br />
+        <div className="image">
+            <img src='src/assets/images.jpeg' alt='pic' width="250px" height="250px"/>
+        </div>
+    </div>
     </>
   )
 }
